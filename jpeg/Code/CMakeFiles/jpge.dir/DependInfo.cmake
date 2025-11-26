3b00120ac4353de3b938dff079697893aabab203
Whoami <78517132+DarkUser1onion@users.noreply.github.com>
2025-11-27 03:41:32 +0700

    Add files via upload

---
+++ jpeg/Code/CMakeFiles/jpge.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/Desktop/jpeg-compressor/jpgd.cpp" "CMakeFiles/jpge.dir/jpgd.cpp.o" "gcc" "CMakeFiles/jpge.dir/jpgd.cpp.o.d"
+  "/home/user/Desktop/jpeg-compressor/jpge.cpp" "CMakeFiles/jpge.dir/jpge.cpp.o" "gcc" "CMakeFiles/jpge.dir/jpge.cpp.o.d"
+  "/home/user/Desktop/jpeg-compressor/tga2jpg.cpp" "CMakeFiles/jpge.dir/tga2jpg.cpp.o" "gcc" "CMakeFiles/jpge.dir/tga2jpg.cpp.o.d"
+  "/home/user/Desktop/jpeg-compressor/timer.cpp" "CMakeFiles/jpge.dir/timer.cpp.o" "gcc" "CMakeFiles/jpge.dir/timer.cpp.o.d"
+  "" "bin/jpge" "gcc" "CMakeFiles/jpge.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ jpeg/Code/CMakeFiles/jpge.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jpge.dir/link.d"
+  "CMakeFiles/jpge.dir/jpgd.cpp.o"
+  "CMakeFiles/jpge.dir/jpgd.cpp.o.d"
+  "CMakeFiles/jpge.dir/jpge.cpp.o"
+  "CMakeFiles/jpge.dir/jpge.cpp.o.d"
+  "CMakeFiles/jpge.dir/tga2jpg.cpp.o"
+  "CMakeFiles/jpge.dir/tga2jpg.cpp.o.d"
+  "CMakeFiles/jpge.dir/timer.cpp.o"
+  "CMakeFiles/jpge.dir/timer.cpp.o.d"
+  "bin/jpge"
+  "bin/jpge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jpge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
