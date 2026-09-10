ea2667efb9bd1bd3f577e61b86711a5f0abaa9b6
DarkUser1onion <guest.void@proton.me>
2026-09-10 22:42:33 +0300

    lvl-1_TaskBoard

--- TaskBoard/Program.cs
+++ TaskBoard/Program.cs
@@ -1,29 +1,58 @@
+using System.Diagnostics;
+using TaskBoard.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<ITaskService, InMemoryTaskService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.Use(async (context, next) =>
+{
+    var sw = Stopwatch.StartNew();
+    context.Response.OnStarting(() =>
+    {
+        sw.Stop();
+        context.Response.Headers.Append("X-Response-Time-ms", sw.ElapsedMilliseconds.ToString());
+        return Task.CompletedTask;
+    });
+    await next(context);
+});
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-App-Name", "TaskBoard");
+    var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("--> {Method} {Path}", context.Request.Method, context.Request.Path);
+    await next(context);
+    logger.LogInformation("<-- {StatusCode}", context.Response.StatusCode);
+});
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/health")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("healthy");
+        return;
+    }
+    await next(context);
+});
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
--- TaskBoard/obj/Debug/net10.0/TaskBoard.AssemblyInfo.cs
+++ TaskBoard/obj/Debug/net10.0/TaskBoard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskBoard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b29d5e7a71ab46f1127bc53798fa07be21bbd602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fafa930640d055107140e93255fadc482772bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskBoard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskBoard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
