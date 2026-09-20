00d41cd382bcd62a37af199aa42e4cb7724559f9
DarkUser1onion <guest.void@proton.me>
2026-09-21 02:07:13 +0300

    lvl-0_ShopCatalog-step2

--- ShopCatalog/obj/Debug/net10.0/ShopCatalog.AssemblyInfo.cs
+++ ShopCatalog/obj/Debug/net10.0/ShopCatalog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopCatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c492c633d07e86f4719cb00d4df92119bf653226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa059c8bf482c2bbabde8142d214e7b8a7e9534e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopCatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopCatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
